d4ac6f078946c83133aa0e46d09cd40bc6c7f492
Evgeny Popov <azenizzka.ru>
2024-04-12 16:58:40 +0700

    init commit

---
+++ src/main/java/ru/azenizzka/fltrans/FlTransApplication.java
@@ -0,0 +1,13 @@
+package ru.azenizzka.fltrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlTransApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlTransApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/azenizzka/fltrans/FlTransApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.azenizzka.fltrans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlTransApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
